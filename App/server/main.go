4ab720c5b53365df01640f2f2a9ff5b2e8ac79fd
Mouhamadou Sylla <syllamouhamed99@gmail.com>
2023-11-17 21:33:59 +0000

    server config

--- App/server/main.go
+++ App/server/main.go
@@ -8,31 +8,7 @@ import (
 const port = ":9092"
 
 func main() {
-	//http.Handle("/App/", http.StripPrefix("/App/", http.FileServer(http.Dir("assets/"))))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../assets/index.html")
-	})
-
-	http.HandleFunc("/profile.png", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../assets/profile.png")
-	})
-
-	// Servir les fichiers statiques (CSS, JavaScript, WebAssembly)
-	http.HandleFunc("/index.css", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../assets/index.css")
-	})
-
-	http.HandleFunc("/script.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../assets/script.js")
-	})
-
-	http.HandleFunc("/main.wasm", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../assets/main.wasm")
-	})
-
-	http.HandleFunc("/wasm_exec.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../assets/wasm_exec.js")
-	})
+	http.Handle("/", http.FileServer(http.Dir("../assets")))
 	log.Printf("Server started on http://localhost%s ...", port)
 	http.ListenAndServe(port, nil)
 }
