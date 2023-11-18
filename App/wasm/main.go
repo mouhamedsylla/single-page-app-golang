cbab55974b743d4cbdc99730ebc7bfa1aa44ea0b
Mouhamadou Sylla <syllamouhamed99@gmail.com>
2023-11-18 19:29:46 +0000

    server modif

--- App/wasm/main.go
+++ App/wasm/main.go
@@ -13,11 +13,9 @@ func main() {
 	router := &framego.Router{}
 	routes := []string{"/", "/posts", "/settings"}
 	pages := []framego.Component{&pages.Dashboard{}, &pages.Posts{}, &pages.Settings{}}
-
 	for i, r := range routes {
 		router.AddRoute(r, pages[i].Render())
 	}
-
 	router.ResolvRoute()
 
 	<-ch
